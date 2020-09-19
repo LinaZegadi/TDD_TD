48b0d6c8b3fa43f33b53a54e6ddb58fb7dc2d400
LinaZegadi <lynamadjda@gmail.com>
2020-09-20 01:30:08 +0200

    Sixth commit

--- src/main/java/fr/parisnanterre/miage/rps/model/RockPaperScissors.java
+++ src/main/java/fr/parisnanterre/miage/rps/model/RockPaperScissors.java
@@ -2,25 +2,56 @@ package fr.parisnanterre.miage.rps.model;
 
    public class RockPaperScissors {
 
-    enum RPSEnum{ROCK, PAPER, SCISSORS};
-    enum Result{WIN, LOST, TIE};
+       enum RPSEnum {ROCK, PAPER, SCISSORS}
 
-     public RockPaperScissors (){}
+       ;
 
-     public Result play(RPSEnum p1, RPSEnum p2)
-     {
-         if(p1 == p2)
-             return Result.TIE;
+       enum Result {WIN, LOST, TIE}
 
-         if ((p1 == RPSEnum.PAPER && p2 == RPSEnum.ROCK) || (p1 == RPSEnum.ROCK && p2 == RPSEnum.SCISSORS) || (p1 == RPSEnum.SCISSORS && p2 == RPSEnum.PAPER))
+       ;
+
+       public RockPaperScissors() {
+       }
+
+       public Result play(RPSEnum p1, RPSEnum p2) {
+           if (p1 == p2)
+               return Result.TIE;
+
+           if ((p1 == RPSEnum.PAPER && p2 == RPSEnum.ROCK) || (p1 == RPSEnum.ROCK && p2 == RPSEnum.SCISSORS) || (p1 == RPSEnum.SCISSORS && p2 == RPSEnum.PAPER))
                return Result.WIN;
 
-         return Result.LOST;
+           return Result.LOST;
 
-     }
+       }
 
-     Result play (Player p1, Player p2)
-     {
-         return  Result.LOST;
-     }
-}
+       Result play(Player p1, Player p2) {
+
+           Result round;
+
+           for (int i = 0; i < p1.getMove(); i++)
+           {
+               round = play(p1.NextMove(), p2.NextMove());
+
+               if(round == Result.WIN)
+                    p1.setScore(p1.getScore()+1);
+
+               else if(round == Result.WIN)
+               {
+                   p1.setScore(p1.getScore() + 1);
+                   p2.setScore(p2.getScore() + 1);
+               }
+
+               else
+                   p2.setScore(p2.getScore() + 1);
+           }
+
+          if(p1.getScore() > p2.getScore())
+              return  Result.WIN;
+
+          else if(p1.getScore() < p2.getScore())
+               return  Result.LOST;
+
+          else
+              return Result.TIE;
+       }
+   }
--- src/test/java/fr/parisnanterre/miage/rps/model/RockPaperScissorsTest.java
+++ src/test/java/fr/parisnanterre/miage/rps/model/RockPaperScissorsTest.java
@@ -2,24 +2,52 @@ package fr.parisnanterre.miage.rps.model;
 
 import org.testng.annotations.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.testng.AssertJUnit.assertEquals;
 
 public class RockPaperScissorsTest {
 
      RockPaperScissors rps;
+     Player  player1, player2, player3;
 
     @BeforeClass
     public void setUp()
     {
         rps = new RockPaperScissors();
+
+        List<RockPaperScissors.RPSEnum> WinMouv = new ArrayList<>();
+        List<RockPaperScissors.RPSEnum> LostMouv = new ArrayList<>();
+
+        assingWin(WinMouv);
+        assingLost(LostMouv);
+        player1 = new Player("player1",WinMouv);
+        player2 = new Player("player2",LostMouv);
+        player3 = new Player("player3",WinMouv);
     }
 
     @AfterClass
     public void tearDown()
     {
         rps = null;
+        player1 = null;
+        player2 = null;
     }
 
+    private void assingWin(List<RockPaperScissors.RPSEnum> m)
+    {
+        m.add(RockPaperScissors.RPSEnum.ROCK);
+        m.add(RockPaperScissors.RPSEnum.PAPER);
+        m.add(RockPaperScissors.RPSEnum.PAPER);
+    }
+
+    private void assingLost(List<RockPaperScissors.RPSEnum> m)
+    {
+        m.add(RockPaperScissors.RPSEnum.SCISSORS);
+        m.add(RockPaperScissors.RPSEnum.ROCK);
+        m.add(RockPaperScissors.RPSEnum.PAPER);
+    }
     @Parameters({"papier" ,"pierre"})
     @Test
     public void testWinPlay(String p1, String p2) {
